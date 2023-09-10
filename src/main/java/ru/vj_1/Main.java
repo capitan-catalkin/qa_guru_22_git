5b2a2bad6af4baddc659a9a03362d8ce7dad38ab
capitan-catalkin <56802737+capitan-catalkin@users.noreply.github.com>
2023-09-10 23:06:19 +0300

    Update Main.java

--- src/main/java/ru/vj_1/Main.java
+++ src/main/java/ru/vj_1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("lkfjhglksfg");
+        System.out.println("   ");
         System.out.println("Hello qa!?");
 
     }
