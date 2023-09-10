1026c3f9b40e608191c314ae071316b9b4b9c6fc
capitan-catalkin <56802737+capitan-catalkin@users.noreply.github.com>
2023-09-10 23:21:30 +0300

    Update Main.java

--- src/main/java/ru/vj_1/Main.java
+++ src/main/java/ru/vj_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     int i = 17;
     public static void main(String[] args) {
 
-        System.out.println("Bye?");
+        System.out.println("Bye777?");
         System.out.println("Hello qa!98765t8 ?");
     }
 }
